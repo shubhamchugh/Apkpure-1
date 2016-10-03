4e429b15b217e5fb93c4b268ad9bfbefe859235c
pengzhendong <275331498@qq.com>
2016-10-03 19:13:52 +0800

    Added exception

--- Apkpure.py
+++ Apkpure.py
@@ -22,17 +22,18 @@ def get_categorys():
 def get_detail_page(arg):
 	for i in range(10):
 		if Mysql.search_apks(arg, i + 1) < 10:
-			tags = get_soup(arg + "?page=" + str(i + 1)).find_all("div", class_="category-template-title")
-			for tag in tags:
-				for link in tag.find_all("a"):
-					Mysql.insert_apks(arg, i + 1, link.attrs['href'])
+			try:
+				tags = get_soup(arg + "?page=" + str(i + 1)).find_all("div", class_="category-template-title")
+				for tag in tags:
+					for link in tag.find_all("a"):
+						Mysql.insert_apks(arg, i + 1, link.attrs['href'])
+			except Exception, e:
+				print e
 
 
 if __name__ == "__main__":
 	# get_categorys()
-	categorys = Mysql.get_all_categorys()
-	for category in categorys:
-		get_detail_page(category)
-		#
-		# for app in apps:
-		# 	print app
+	# categorys = Mysql.get_all_categorys()
+	# for category in categorys:
+	#	get_detail_page(category)
+	print "hello"
